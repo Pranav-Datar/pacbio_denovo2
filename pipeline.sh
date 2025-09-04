4f0b2e5a294ad97661fb4a10a804b0509d68439f
Pranav-Datar <pranavdatar01@gmail.com>
2025-09-04 21:46:29 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -5,5 +5,9 @@ prefetch SRR8797220 --output-directory /home/adminiisc/komodo_sra
 fasterq-dump SRR8797220.sra
 #creates fastq file from sra file
 
-gzip SRR8797220.sra.fastq
-#converts fastq to fq.gz
+#Now to gunzip it, do the following
+conda install -c conda-forge pigz
+pv SRR8797220.sra.fastq | pigz -c > SRR8797220.sra.fastq.gz
+#pv: pipe viewer
+#pigz = parallel implementation of gzip
+#-c: write the compressed data to standard output (stdout), instead of replacing the original file
