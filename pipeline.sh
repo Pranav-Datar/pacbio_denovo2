f0bd339fa1926519a03512d7698c5e78c0e64d3a
Pranav-Datar <pranavdatar01@gmail.com>
2025-09-04 21:35:49 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,9 @@
 conda activate sratoolkit
 prefetch SRR8797220 --output-directory /home/adminiisc/komodo_sra
 #the sra file is downloaded in /home/adminiisc/komodo_sra
+
+fasterq-dump SRR8797220.sra
+#creates fastq file from sra file
+
+gzip SRR8797220.sra.fastq
+#converts fastq to fq.gz
