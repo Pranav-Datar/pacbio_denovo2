8c5b7bc564dde89bf3d19cdf193f38bcc2699b8f
Pranav-Datar <pranavdatar01@gmail.com>
2025-09-05 11:21:24 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -11,3 +11,17 @@ pv SRR8797220.sra.fastq | pigz -c > SRR8797220.sra.fastq.gz
 #pv: pipe viewer
 #pigz = parallel implementation of gzip
 #-c: write the compressed data to standard output (stdout), instead of replacing the original file
+
+#create read length table
+echo "platform,length" > length.csv
+#creates platform and length columns in length.csv file
+
+conda install bioconda::bioawk
+
+pv SRR8797220.sra.fastq.gz | bioawk -c fastx '{print "PacBio_HiFi," length($seq)}' >> length.csv
+#pv: pipe viewer
+#pv SRR8797220.sra.fastq.gz: pv reads the zipped fastq file
+#bioawk: version of awk that understands fasta/fastq files
+#-c fastx: parses the input as fasta/fastq
+#length($seq) = number of bases in that read.
+#"PacBio_HiFi," is just a label (hardcoded string)
