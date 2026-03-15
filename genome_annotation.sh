214b0223c14a6a61d97de939a5b6e239d69cb899
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-15 16:14:35 +0530

    Update genome_annotation.sh

--- genome_annotation.sh
+++ genome_annotation.sh
@@ -5,3 +5,13 @@ BuildDatabase -name Vkomo_db GCF_004798865.1_ASM479886v1_genomic.fasta
 #BuildDatabase: program included within RepeatModeler
 #-name Vkomo_db: constructs database "Vkomo_db"
 #GCF_004798865.1_ASM479886v1_genomic.fasta: input genome sequence
+
+#output:
+#Vkomo_db.nsq: contains the actual genome sequence data stored in database format
+#Vkomo_db.nhr: contains sequence metadata
+#Vkomo_db.nin: stores the index of sequences 
+#Vkomo_db.nnd: additional indexing information
+#Vkomo_db.nni: nucleotide index index
+#Vkomo_db.nog: sequence grouping information
+#Vkomo_db.njs: job scheduling metadata
+#Vkomo_db.translation: contains translation information
