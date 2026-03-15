2730a0e1b31a3538aa989edc7dee7a0311e46c5a
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-15 16:05:03 +0530

    Add comments for BuildDatabase command
    Added comments to explain the BuildDatabase command in genome_annotation.sh.

--- genome_annotation.sh
+++ genome_annotation.sh
@@ -1,1 +1,7 @@
 conda create -n repeatmodeler -c bioconda -c conda-forge repeatmodeler
+
+BuildDatabase -name Vkomo_db GCF_004798865.1_ASM479886v1_genomic.fasta
+#converts genome.fasta into an indexed database, and prepares it for fast discovery
+#BuildDatabase: program included within RepeatModeler
+#-name Vkomo_db: constructs database "Vkomo_db"
+#GCF_004798865.1_ASM479886v1_genomic.fasta: input genome sequence
