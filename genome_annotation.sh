ca45046b7f8315fdaad337af20501a7899a53bea
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-17 15:37:58 +0530

    Update genome_annotation.sh

--- genome_annotation.sh
+++ genome_annotation.sh
@@ -1,5 +1,5 @@
 conda create -n repeatmodeler -c bioconda -c conda-forge repeatmodeler
-
+conda activate repeatmodeler
 BuildDatabase -name Vkomo_db GCF_004798865.1_ASM479886v1_genomic.fasta
 #converts genome.fasta into an indexed database, and prepares it for fast discovery
 #BuildDatabase: program included within RepeatModeler
