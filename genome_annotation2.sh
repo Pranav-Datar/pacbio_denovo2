572d59ddaca3f75d4b141a95993bee9bd86724be
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-17 17:41:27 +0530

    Update genome_annotation2.sh

--- genome_annotation2.sh
+++ genome_annotation2.sh
@@ -1,1 +1,5 @@
+conda create -n repeatmodeler -c bioconda -c conda-forge repeatmodeler
+conda activate repeatmodeler
+BuildDatabase -name Vkomo_db GCF_004798865.1_ASM479886v1_genomic.fasta 2>&1 | tee repeatmodeler.log
 
+wget https://www.dfam.org/releases/Dfam_3.8/families/Dfam.embl.gz
