d8e3a4faaf2ac31c496e8a7d88d5cca5fe528e5b
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-18 12:03:22 +0530

    Update genome_annotation2.sh

--- genome_annotation2.sh
+++ genome_annotation2.sh
@@ -3,4 +3,17 @@ conda activate repeatmodeler
 BuildDatabase -name Vkomo_db GCF_004798865.1_ASM479886v1_genomic.fasta 2>&1 | tee repeatmodeler.log
 RepeatModeler -threads 40 -database Vkomo_db/Vkomo_db 2>&1 | tee repeatmodeler.log
 
-wget https://www.dfam.org/releases/Dfam_3.8/families/Dfam.embl.gz
+#download transcriptome data for komodo dragon
+#download genome annotation file (gff) of komodo dragon from ncbi
+gffread GCF_004798865.1_ASM479886v1_genomic.gff -T -o annotation.gtf
+
+#transcriptome alignment and assembly
+conda create -n star_env -c bioconda -c conda-forge star
+conda activate star_env
+STAR \
+--runThreadN 50 \
+--runMode genomeGenerate \
+--genomeDir star_index \
+--genomeFastaFiles genome/V_komo_genome.fasta \
+--sjdbGTFfile genome/annotation.gtf \
+--sjdbOverhang 149
