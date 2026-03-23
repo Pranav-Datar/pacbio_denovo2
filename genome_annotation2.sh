b81bc973a89b287feb20e1924a74fd3c99a6d874
Pranav-Datar <pranavdatar01@gmail.com>
2026-03-23 11:36:23 +0530

    Update genome_annotation2.sh

--- genome_annotation2.sh
+++ genome_annotation2.sh
@@ -45,6 +45,8 @@ Trinity \
 
 #make sure that there is no selenocysteine (U) amino acid in it. if its there, replace it with (C), which is the closest and workable substitute
 
+makeblastdb -in V_komo_genome.fasta -dbtype nucl -out komo_blastdb -max_file_sz 4GB
+
 #for aligning protein sequences to komodo genome,
 seqkit split -s 500 combined_reptile_proteins.clean.faa -O chunks/
 
