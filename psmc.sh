6fe3f049de8724067830951c6a1660a293632c9a
Pranav-Datar <pranavdatar01@gmail.com>
2026-04-22 23:21:49 +0530

    Update psmc.sh

--- psmc.sh
+++ psmc.sh
@@ -14,3 +14,15 @@ pbmm2 align reference.mmi SRR8797220.fastq V_komo_aligned.bam --preset HIFI --so
 #Now, variant calling. But before that, ensure that you have Sorted BAM, Indexed BAM (.bai), Reference FASTA and Indexed FASTA (.fai)
 conda activate longshot_env
 longshot --bam V_komo_aligned.bam --ref V_komo_genome.fasta --out variants.vcf
+
+conda activate bcftools_env
+
+bgzip variants.vcf
+tabix -p vcf variants.vcf.gz
+
+bcftools filter \
+>   -i 'QUAL>=30 && FORMAT/DP>=4 && FORMAT/GQ>=10' \
+>   variants.vcf.gz \
+>   -Oz -o variants.filtered.vcf.gz
+
+tabix -p vcf variants.filtered.vcf.gz
