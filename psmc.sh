90c7e1fc4c1ba8d96e50ff96140b173bc25a1f84
Pranav-Datar <pranavdatar01@gmail.com>
2026-04-22 19:21:49 +0530

    Update psmc.sh

--- psmc.sh
+++ psmc.sh
@@ -4,3 +4,9 @@ conda activate pbmm2_env
 pbmm2 index V_komo_genome.fasta reference.mmi --preset HIFI
 #V_komo_genome.fasta: input genome
 #reference.mmi: output index
+
+#aligning the raw pacbio reads to the genome
+pbmm2 align reference.mmi SRR8797220.fastq V_komo_aligned.bam --preset HIFI --sort
+#SRR8797220.fastq: input reads
+#V_komo_aligned_bam: output
+#--sort: for sorting
