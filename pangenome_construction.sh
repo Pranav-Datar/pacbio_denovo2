b325bec1f2c650433ac5b3414e78ba2403e2959f
Pranav-Datar <pranavdatar01@gmail.com>
2026-07-29 23:29:07 +0530

    Update pangenome_construction.sh

--- pangenome_construction.sh
+++ pangenome_construction.sh
@@ -3,49 +3,55 @@ seqkit replace \
 -p '^(\S+)' \
 -r 'acanthurus#0#${1}' \
 /home/pranav/genome_assemblies/NCBI_data/V_acanthurus_ncbi/GCA_050042745.1_ZJU_Vac_2.0_genomic.fasta \
-> acanthurus.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/acanthurus.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'komodoensis#0#${1}' \
 /home/pranav/genome_assemblies/NCBI_data/V_komodoensis_ncbi/genome/V_komo_genome.fasta \
-> komodoensis.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/komodoensis.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'salvator#0#${1}' \
 /home/pranav/genome_assemblies/NCBI_data/V_salvator_2_ncbi/genome/GWHHKDC00000000.1.genome.fasta \
-> salvator.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/salvator.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'brevicauda#1#${1}' \
 /home/pranav/genome_assemblies/primary_data/V_brevicauda/brevicauda_combined_reanalysis2/assembly_files/brevicauda.assembly.bp.hap1.p_ctg.fasta \
-> brevicauda.hap1.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/brevicauda.hap1.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'brevicauda#2#${1}' \
 /home/pranav/genome_assemblies/primary_data/V_brevicauda/brevicauda_combined_reanalysis2/assembly_files/brevicauda.assembly.bp.hap2.p_ctg.fasta \
-> brevicauda.hap2.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/brevicauda.hap2.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'panoptes#1#${1}' \
 /home/pranav/genome_assemblies/primary_data/V_panoptes/panoptes_reanalysis2/assembly_files/panoptes.assembly.bp.hap1.p_ctg.fasta \
-> panoptes.hap1.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/panoptes.hap1.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'panoptes#2#${1}' \
 /home/pranav/genome_assemblies/primary_data/V_panoptes/panoptes_reanalysis2/assembly_files/panoptes.assembly.bp.hap2.p_ctg.fasta \
-> panoptes.hap2.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/panoptes.hap2.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
 -r 'scalaris#1#${1}' \
 /home/pranav/genome_assemblies/primary_data/V_scalaris/scalaris_reanalysis2/assembly_files/scalaris.assembly.bp.hap1.p_ctg.fasta \
-> scalaris.hap1.pansn.fa
+> /home/pranav/genome_assemblies/pangenome_input/scalaris.hap1.pansn.fa
+
+seqkit replace \
+-p '^(\S+)' \
+-r 'scalaris#2#${1}' \
+/home/pranav/genome_assemblies/primary_data/V_scalaris/scalaris_reanalysis2/assembly_files/scalaris.assembly.bp.hap2.p_ctg.fasta \
+> /home/pranav/genome_assemblies/pangenome_input/scalaris.hap2.pansn.fa
 
 seqkit replace \
 -p '^(\S+)' \
